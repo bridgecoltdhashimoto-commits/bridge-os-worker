65b0f9f2d40dd56ebf64c94130229c266c04ce7c
bridgecoltdhashimoto-commits <224437397+bridgecoltdhashimoto-commits@users.noreply.github.com>
2026-03-05 11:03:24 -0800

    RegPatch: Emergency fix

--- index.js
+++ index.js
@@ -1,15 +1,1 @@
-
-    export default {
-      async fetch(request, env) {
-        const body = await request.text();
-        const payload = { ...JSON.parse(body), status: "ENQUEUED" };
-        // GAS??????????????????????????
-        await fetch("https://script.google.com/macros/s/AKfycbyvrvT1wHGYi7E7snBNsxEcSX5MHmQO31ZYugKDNe4TSo2M178m1zIztkbvMOL8NO48/exec", {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify(payload)
-        });
-        return new Response("OK", { status: 200 });
-      }
-    };
-  
+export default { async fetch(r,e){ const b=await r.text(); const p={...JSON.parse(b),status:"ENQUEUED"}; await fetch("https://script.google.com/macros/s/AKfycbyvrvT1wHGYi7E7snBNsxEcSX5MHmQO31ZYugKDNe4TSo2M178m1zIztkbvMOL8NO48/exec",{method:"POST",headers:{"Content-Type":"application/json"},body:JSON.stringify(p)}); return new Response("OK"); } };
